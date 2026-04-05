0a637b5858f056b1dce2e2ce790c8d0a2436f1a1
Santtana <santtana@proton.me>
2026-04-05 22:57:52 +0200

    fix: PWA icons maskable + cache all assets for install

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'estadsalud-v3';
-const FILES = ['./', './index.html', './manifest.json'];
+const CACHE_NAME = 'estadsalud-v4';
+const FILES = ['./', './index.html', './manifest.json', './icon-192.png', './icon-512.png'];
 
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));

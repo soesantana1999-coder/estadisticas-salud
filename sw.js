a240956de06b9121725a91a0c46d4fca9fb02239
Santtana <santtana@proton.me>
2026-04-05 22:08:26 +0200

    EstadSalud Cuba v1.0 - PWA offline para estadisticos de salud

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'estadsalud-v1';
+const FILES = ['/', '/index.html', '/manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request).then(resp => {
+      const clone = resp.clone();
+      caches.open(CACHE_NAME).then(c => c.put(e.request, clone));
+      return resp;
+    }).catch(() => caches.match('/index.html')))
+  );
+});
